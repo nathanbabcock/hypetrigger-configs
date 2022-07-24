5bc647a16f4829134c772355207bdebef45291c1
nbabcock <nathan.r.babcock@gmail.com>
2022-07-24 17:26:44 -0500

    chore(version): 0.11.0

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import type Trigger from '../triggers/trigger'
 export type Config = {
-  id: string,
-  title: string,
-  exe?: string,
-  igdbGameId?: number,
-  triggers: Trigger[],
+  id: string
+  title: string
+  exe?: string
+  version?: number
+  igdbGameId?: number
+  triggers: Trigger[]
 }
